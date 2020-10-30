38e6708a30dc04101c56e8e3a5b0bd1ae01ec958
devan5hu <52581680+devan5hu@users.noreply.github.com>
2020-10-31 04:39:01 +0530

    Classes for Students Updated

---
+++ lib/screens/MarkAttendanceStudents.dart
@@ -0,0 +1,16 @@
+import 'constants.dart';
+import 'package:flutter/material.dart';
+
+class MarkAttendanceStudents extends StatefulWidget {
+  @override
+  _MarkAttendanceStudentsState createState() => _MarkAttendanceStudentsState();
+}
+
+class _MarkAttendanceStudentsState extends State<MarkAttendanceStudents> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body : Text("Yet to be implemented",)
+    );
+  }
+}
