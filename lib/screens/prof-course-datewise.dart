1ec5d9f27c9e48b7b884cd30b56e2a2ad0215c28
Angad Kambli <angad-k@users.noreply.github.com>
2020-11-15 13:59:38 +0530

    remove some hardcodes

--- lib/screens/prof-course-datewise.dart
+++ lib/screens/prof-course-datewise.dart
@@ -7,11 +7,30 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 class DatewiseClassDeets extends StatefulWidget {
+  String lecDate;
+  int TotAttended, TotStudents;
+  DatewiseClassDeets(tot_attended, total, date)
+  {
+    TotAttended = tot_attended;
+    TotStudents = total;
+    lecDate = date;
+  }
+
   @override
-  _DatewiseClassDeetsState createState() => _DatewiseClassDeetsState();
+  _DatewiseClassDeetsState createState() => _DatewiseClassDeetsState(TotAttended, TotStudents, lecDate);
 }
 
 class _DatewiseClassDeetsState extends State<DatewiseClassDeets> {
+
+  String lecDate;
+  int TotAttended, TotStudents;
+  _DatewiseClassDeetsState(tot_attended, total, date)
+  {
+    TotAttended = tot_attended;
+    TotStudents = total;
+    lecDate = date;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,10 +38,10 @@ class _DatewiseClassDeetsState extends State<DatewiseClassDeets> {
         backgroundColor: PrimaryColor,
         centerTitle: true,
         toolbarHeight: 100,
-        title: Text("Date"),
+        title: Text("Lecture date : " + lecDate),
     ),
     body:  Container(
-    child: ShowdataProfCourse(),
+    child: ShowdataProfCourse(TotAttended, TotStudents),
     ),
     drawer: account_drawer(
     Name: "Angad Kambli" + ", " + "19114041",
@@ -34,20 +53,34 @@ class _DatewiseClassDeetsState extends State<DatewiseClassDeets> {
   }
 }
 class ShowdataProfCourse extends StatefulWidget {
+
+  int TotAttended, TotStudents;
+  ShowdataProfCourse(tot_attended, total)
+  {
+    TotAttended = tot_attended;
+    TotStudents = total;
+  }
+
   @override
-  _ShowdataProfCourseState createState() => _ShowdataProfCourseState();
+  _ShowdataProfCourseState createState() => _ShowdataProfCourseState(TotAttended, TotStudents);
 }
 
 class _ShowdataProfCourseState extends State<ShowdataProfCourse> {
   @override
+
+  int TotAttended, TotStudents;
+  _ShowdataProfCourseState(tot_attended, total)
+  {
+    TotAttended = tot_attended;
+    TotStudents = total;
+  }
+
   Widget build(BuildContext context) {
 
     int decimals = 2;
     int fac = pow(10, decimals);
     double d = 1.234567889;
     d = (d * fac).round() / fac;
-    final int TotStudents = 100;
-    var TotAttended = 20;
     double ratio_attended = (TotAttended/TotStudents * fac).round() / fac;
     var bar_color;
     if(ratio_attended > 0.5) {
