5bd03e1ece0e1b07013fc7fee6653278501558ae
devan5hu <52581680+devan5hu@users.noreply.github.com>
2020-11-15 13:28:34 +0530

    Created a radial gauge for Prof Side

--- lib/screens/prof-course-datewise.dart
+++ lib/screens/prof-course-datewise.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/rendering.dart';
 import 'constants.dart';
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 
 class DatewiseClassDeets extends StatefulWidget {
   @override
@@ -41,9 +41,94 @@ class ShowdataProfCourse extends StatefulWidget {
 class _ShowdataProfCourseState extends State<ShowdataProfCourse> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Title"),
+
+    int decimals = 2;
+    int fac = pow(10, decimals);
+    double d = 1.234567889;
+    d = (d * fac).round() / fac;
+    final int TotStudents = 100;
+    var TotAttended = 20;
+    double ratio_attended = (TotAttended/TotStudents * fac).round() / fac;
+    var bar_color;
+    if(ratio_attended > 0.5) {
+      bar_color = Colors.green;
+    }
+    else {
+      bar_color = Colors.red;
+    }
+    var percentage = (ratio_attended)*100;
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: size.height*0.5,
+          child: Stack(
+            children: <Widget>[
+              Center(
+                child: Container(
+                  width: size.height*0.3,
+                  height: size.height*0.3,
+                  child: new CircularProgressIndicator(
+                    strokeWidth: 15,
+                    value: ratio_attended,
+                    valueColor: new AlwaysStoppedAnimation<Color>(bar_color),
+                  ),
+                ),
+              ),
+              Center(child:
+              Text("${percentage}% attendance"),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          height: 24,
+          child: Stack(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text("Total Students", style: TextStyle(fontSize: 20 , fontWeight: FontWeight.bold),),
+              )
+            ],
+          ),
+        ),
+        Container(
+          height: 24,
+          child: Stack(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text("${TotStudents}" , style: TextStyle(fontSize: 18),),
+              )
+            ],
+          ),
+        ),
+
+        Container(
+          height: 24,
+          child: Stack(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text("Total Class Attendance ", style: TextStyle(fontSize: 20 , fontWeight: FontWeight.bold, ),),
+                )
+            ],
+          ),
+        ),
+        Container(
+          height: 24,
+          child: Stack(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text("${TotAttended}" , style: TextStyle(fontSize: 18),),
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
+
 
